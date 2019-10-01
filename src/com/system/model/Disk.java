71e80d20c06b3c0ccfe0698c8afba992f8b4d6df
罗俊权 <masonluo918@gmail.com>
2019-10-01 09:12:03 +0800

    添加了更详细的磁盘的写入功能

--- src/com/system/model/Disk.java
+++ src/com/system/model/Disk.java
@@ -1,5 +1,6 @@
 package com.system.model;
 
+import com.system.common.observer.DiskObserver;
 import com.system.utils.FileUtils;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -17,9 +18,6 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * @author masonluo
  */
 public class Disk extends Observable {
-
-    //TODO 更灵活的读取和写入磁盘方式
-    private static final long serialVersionUID = 1L;
 
     /*    1、分配内存空间
           2、初始化对象
@@ -58,6 +56,7 @@ public class Disk extends Observable {
         }
         // 加载文件内容
         loadDisk(file);
+        loadObserver();
         notifyObserver();
     }
     /**
@@ -72,6 +71,121 @@ public class Disk extends Observable {
             }
         }
         return instance;
+    }
+    /**
+     * 通知观察者，磁盘已经发生变化
+     */
+    public void notifyObserver(){
+        setChanged();
+        notifyObservers();
+    }
+    /**
+     * 将space的数据持久化到文件中
+     */
+    public void persistDisk() {
+        File file = new File(FILE_PATH);
+        try {
+            FileUtils.saveBytes(space, file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+    }
+    /**
+     * 从一个文件加载磁盘
+     * @param file
+     */
+    public void loadDisk(File file){
+        space = new byte[BLOCK_NUM][BLOCK_SIZE];
+        try{
+            FileUtils.loadBytes(space, file);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    /**
+     * 获取一个索引值为i的磁盘块
+     * @param index 索引值
+     * @return 磁盘块的复制，避免外部直接操纵磁盘
+     */
+    public byte[] readBlock(int index){
+        if(index > BLOCK_NUM){
+            throw new IndexOutOfBoundsException("index error");
+        }
+        // 传回去一个复制了的数组，避免外部直接操纵磁盘的数组
+        readLock.lock();
+        try{
+            return Arrays.copyOf(space[index], space[index].length);
+        }finally {
+            readLock.unlock();
+        }
+    }
+    /**
+     * 写一个磁盘块
+     * @param index 索引值
+     * @param bytes 要写入的内容
+     */
+    public void writeBlock(int index, byte[] bytes){
+        if(index > BLOCK_NUM || bytes.length > BLOCK_SIZE){
+            throw new IndexOutOfBoundsException("index error");
+        }
+        writeLock.lock();
+        try{
+            // 避免外部的数组直接操纵磁盘
+            space[index] = Arrays.copyOf(bytes, bytes.length);
+            // 通知观察者，磁盘变化
+            notifyObserver();
+        }finally {
+            writeLock.unlock();
+        }
+    }
+
+    /**
+     * 写一个磁盘块， 从index的第0个字节，写入writeNum个字节，对应bytes的第0个，
+     * 到第writeNum个字节
+     * @param index 磁盘块索引
+     * @param writeNum 写入的个数
+     * @param bytes 要写入的字节数
+     */
+    public void writeBlock(int index, int writeNum,  byte[] bytes){
+        if(writeNum > BLOCK_SIZE){
+            throw new IndexOutOfBoundsException("index should be less than " + BLOCK_SIZE);
+        }
+        writeLock.lock();
+        try{
+            for(int i = 0; i < writeNum; i++){
+                space[index][i] = bytes[i];
+            }
+        }finally {
+            writeLock.unlock();
+            notifyObserver();
+        }
+    }
+
+    /**
+     * 写入一个磁盘块
+     * @param index 写入的磁盘块的索引
+     * @param startIndex 写入的第startIndex个字节
+     * @param endIndex 写入截止到第endIndex个字节（不包括endIndex）
+     * @param bytes 要写入的数据
+     */
+    public void writeBlock(int index, int startIndex, int endIndex, byte[] bytes){
+        if(bytes.length > endIndex - startIndex){
+            throw new RuntimeException("The length of byte array does not match");
+        }
+        if(endIndex > BLOCK_SIZE){
+            throw new IndexOutOfBoundsException("endIndex should be less than " + BLOCK_SIZE);
+        }
+        writeLock.lock();
+        try{
+            for(int i = startIndex; i < endIndex; i++){
+                space[index][i] = bytes[i - startIndex];
+            }
+        }finally {
+            writeLock.unlock();
+            notifyObserver();
+        }
+
     }
     /**
      * 如果判断文件为空，执行磁盘的首次初始化，初始化byte数组，
@@ -110,74 +224,10 @@ public class Disk extends Observable {
         space[0][0] = END_FILE_MARK;
         space[0][1] = END_FILE_MARK;
     }
-    /**
-     * 通知观察者，磁盘已经发生变化
-     */
-    public void notifyObserver(){
-        setChanged();
-        notifyObservers();
+    private void loadObserver(){
+        DiskObserver observer = new DiskObserver();
+        addObserver(observer);
     }
-    /**
-     * 将space的数据持久化到文件中
-     */
-    public void persistDisk() {
-        File file = new File(FILE_PATH);
-        try {
-            FileUtils.saveBytes(space, file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println();
-    }
-    /**
-     * 从一个文件加载磁盘
-     * @param file
-     */
-    public void loadDisk(File file){
-        space = new byte[BLOCK_NUM][BLOCK_SIZE];
-        try{
-            FileUtils.loadBytes(space, file);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-    /**
-     * 获取一个索引值为i的磁盘块
-     * @param i 索引值
-     * @return 磁盘块的复制，避免外部直接操纵磁盘
-     */
-    public byte[] readBlock(int i){
-        if(i > BLOCK_NUM){
-            throw new IndexOutOfBoundsException("index error");
-        }
-        // 传回去一个复制了的数组，避免外部直接操纵磁盘的数组
-        readLock.lock();
-        try{
-            return Arrays.copyOf(space[i], space[i].length);
-        }finally {
-            readLock.unlock();
-        }
-    }
-    /**
-     * 写一个磁盘块
-     * @param i 索引值
-     * @param bytes 要写入的内容
-     */
-    public void writeBlock(int i, byte[] bytes){
-        if(i > BLOCK_NUM || bytes.length > BLOCK_SIZE){
-            throw new IndexOutOfBoundsException("index error");
-        }
-        writeLock.lock();
-        try{
-            // 避免外部的数组直接操纵磁盘
-            space[i] = Arrays.copyOf(bytes, bytes.length);
-            // 通知观察者，磁盘变化
-            notifyObserver();
-        }finally {
-            writeLock.unlock();
-        }
-    }
-
     public static int getBlockSize() {
         return BLOCK_SIZE;
     }
